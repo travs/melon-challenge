9a3879fdfced300f72bcde29379064f222b62050
Travis Jacobs <traviswjacobs@gmail.com>
2017-03-31 18:49:19 -0230

    Small config changes; revise from account for testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var CrowdSale = artifacts.require("./CrowdSale.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(CrowdSale, {overwrite: true});
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(CrowdSale, 24, {overwrite: true, from: accounts[0]});
 };
